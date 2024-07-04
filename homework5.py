e4f35418c7125e83fe9d1bf2b1c6ea52a90554cf
Anna-Andr-new <tsukanova0910anna@gmail.com>
2024-07-04 16:26:11 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,6 @@
+tupple_immutable_var= 'cat', 'dog','frog',1,65
+print(tupple_immutable_var)
+print(tupple_immutable_var,+ 2)
+mutable_list= ('apple, banana, cucumber')
+print (mutable_list)
+print (mutable_list[0:2:4]+'n',mutable_list)
